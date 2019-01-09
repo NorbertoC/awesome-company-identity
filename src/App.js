d9f7a98ec61e7e3355d69625cce4dd790e06876b
Alfredw <coomcs@gmail.com>
2019-01-09 20:07:19 +1300

    Cleaning styles and added fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/header';
+import FirstSection from './components/first-section/first-section';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <FirstSection />
       </div>
     );
   }
---
+++ src/components/first-section/first-section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './first-section.css';
+
+const FirstSection = () => (
+  <div className="first-section-container">
+    <p>Hello, world!</p>
+    <h1>Hello, world!</h1>
+    <h2>Hello, world!</h2>
+  </div>
+);
+
+export default FirstSection;
